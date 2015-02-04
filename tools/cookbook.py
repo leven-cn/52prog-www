915aed7dbfcca1bf174e4c108ee57804f822d5cd
Li Yun <leven.cn@gmail.com>
2015-02-04 14:35:04 +0800

    Remove `SocketTestCase` from `cookbook` tool.

--- tools/cookbook.py
+++ tools/cookbook.py
@@ -18,12 +18,7 @@ limitations under the License.
 '''
 
 import sys
-import socket
 import unittest
-try:
-    from unittest.mock import patch
-except ImportError:
-    pass
 
 import pep8
 
@@ -34,7 +29,7 @@ class GeneralTestCase(unittest.TestCase):
     Includes:
 
         - Python version
-        - PEP 8 conformance
+        - PEP 8 conformance (pep8 required)
 
     '''
 
@@ -45,13 +40,8 @@ class GeneralTestCase(unittest.TestCase):
         self.pep8_quiet = False
 
     def test_py_version_conformance(self):
-        # Python 2.7.9+ or 3.4+ required
-        if sys.version_info.major == 2:
-            self.assertGreater(sys.version_info, (2, 7, 9),
-                               'Python 2.7.9+ required')
-        elif sys.version_info.major == 3:
-            self.assertGreater(sys.version_info, (3, 4),
-                               'Python 3.4+ required')
+        self.assertGreaterEqual(sys.version_info, (3, 4),
+                                'Python 3.4+ required')
 
     def test_pep8_conformance(self):
         pep8_style = pep8.StyleGuide(quiet=self.pep8_quiet)
@@ -59,49 +49,6 @@ class GeneralTestCase(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          'Found {0} code style errors (and warnings)'
                          .format(result.total_errors))
-
-
-@unittest.skipUnless(sys.version_info > (3, 3),
-                     'unittest.mock module support, Python 3.3+ required')
-class SocketTestCase(unittest.TestCase):
-    '''Test case for BSD socket APIs.
-
-    Includes:
-
-        - TCP
-        - UDP
-
-    '''
-
-    def setUp(self):
-        self.patcher = patch('socket.socket', autospec=True)
-        self.MockSocket = self.patcher.start()
-
-    def tearDown(self):
-        self.patcher.stop()
-
-    def test_mocksocket(self):
-        self.assertIs(self.MockSocket, socket.socket)
-
-        sock = self.MockSocket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.socket.assert_called_once_with(socket.AF_INET,
-                                              socket.SOCK_STREAM)
-        sock.close()
-        sock.close.assert_called_once_with()
-
-    def tcp_socket_handle(self):
-        pass
-
-    def test_tcp_socket(self):
-        with self.MockSocket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            self.tcp_socket_handle()
-
-    def udp_socket_handle(self):
-        pass
-
-    def test_udp_socket(self):
-        with self.MockSocket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-            self.udp_socket_handle()
 
 
 if __name__ == '__main__':
