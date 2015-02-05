044089aa97323487d59480bac353b018efe62925
Li Yun <leven.cn@gmail.com>
2015-02-05 13:36:18 +0800

    Add tool `cookbook2` for Python 2.7.9+.

---
+++ tools/cookbook2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+'''Python 2 Cookbook.
+
+Copyright 2015 Li Yun.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+import sys
+import unittest
+
+import pep8
+
+
+class GeneralTestCase(unittest.TestCase):
+    '''General test case for module.
+
+    Includes:
+
+        - Python version
+        - PEP 8 conformance (pep8 required)
+
+    '''
+
+    def setUp(self):
+        '''Subclasses must provide `test_modules` and could configure it by
+        override this method.'''
+        self.test_modules = [__file__]
+        self.pep8_quiet = False
+
+    def test_py_version_conformance(self):
+        self.assertGreaterEqual(sys.version_info, (2, 7, 9),
+                                'Python 2.7.9+ required')
+
+    def test_pep8_conformance(self):
+        pep8_style = pep8.StyleGuide(quiet=self.pep8_quiet)
+        result = pep8_style.check_files(self.test_modules)
+        self.assertEqual(result.total_errors, 0,
+                         'Found {0} code style errors (and warnings)'
+                         .format(result.total_errors))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2, catchbreak=True)
