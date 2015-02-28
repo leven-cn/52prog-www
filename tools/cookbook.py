e74ef9087e8d939eaa1af29e3e447fce306acf71
Li Yun <leven.cn@gmail.com>
2015-02-28 16:08:33 +0800

    Minor fix copyright statements for Python Cookbook.

--- tools/cookbook.py
+++ tools/cookbook.py
@@ -2,7 +2,7 @@
 
 '''Python Cookbook.
 
-Copyright 2015 Li Yun.
+Copyright 2015 Li Yun <leven.cn@gmail.com>.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
